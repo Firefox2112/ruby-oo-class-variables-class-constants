98ad4a537333d876da0586d0b07c89f4e2e599fa
Coby <cobyrpilpel2@gmail.com>
2020-12-18 03:42:31 -0500

    commit tree 187fbda6390f9904f3120eaba14c7387cd8a55a5

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,28 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  attr_accessor :color, :size, :material, :condition, :brand
+  #attr_reader #:brand
+
+BRANDS = []
 
   def initialize(brand)
     @brand = brand
+  if !BRANDS.include?(brand)
+    BRANDS << brand
   end
+end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
 
+  #def brand=(brand)
+    #@brand = brand
+    #BRANDS << brand
+ # end
+
+
+
 end
+
+#I think I get it why this passed. Don't know why ## out didn't pass.  Ask M on saturday. 
